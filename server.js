6d646f6705d4b12fcf2b43b1573546dff15b2f85
one07 <heryfidiawan07@gmail.com>
2017-02-28 15:31:34 +0700

    add port and procfile

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app  = require('express')();
 var http = require('http').Server(app);
 var io   = require('socket.io')(http);
 
+var port = process.env.PORT || 8080
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -45,6 +47,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('Listening on 3000......');
+http.listen(port, function(){
+	console.log('Listening on 8080......');
 });
